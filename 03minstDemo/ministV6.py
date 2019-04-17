3f290f98003fc99cc000df9b5d7bf5f0c3e51b60
zym <843024750@qq.com>
2019-04-17 16:29:32 +0800

    AlexNet

--- 03minstDemo/ministV6.py
+++ 03minstDemo/ministV6.py
@@ -2,20 +2,20 @@ from __future__ import print_function
 import numpy as np
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import SGD
+from keras.layers.core import Dense, Dropout, Activation,Flatten
+from keras.optimizers import RMSprop
 from keras.utils import np_utils
-
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 import matplotlib.pyplot as plt
 
 np.random.seed(1671)  # for reproducibility
 
 # network and training
-NB_EPOCH = 250
+NB_EPOCH = 20
 BATCH_SIZE = 128
 VERBOSE = 1
 NB_CLASSES = 10   # number of outputs = number of digits
-OPTIMIZER = SGD() # optimizer, explained later in this chapter
+OPTIMIZER = RMSprop() # optimizer, explainedin this chapter
 N_HIDDEN = 128
 VALIDATION_SPLIT=0.2 # how much TRAIN is reserved for VALIDATION
 DROPOUT = 0.3
@@ -26,8 +26,8 @@ DROPOUT = 0.3
 #X_train is 60000 rows of 28x28 values --> reshaped in 60000 x 784
 RESHAPED = 784
 #
-X_train = X_train.reshape(60000, RESHAPED)
-X_test = X_test.reshape(10000, RESHAPED)
+X_train = X_train.reshape(60000, 28,28,1)
+X_test = X_test.reshape(10000, 28,28,1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
@@ -46,14 +46,29 @@ Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
 # final stage is softmax
 
 model = Sequential()
-model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
+model.add(Conv2D(32, kernel_size=(5,5),strides=2, padding='same',
+                        input_shape=(28, 28, 1)))#32个卷积核 ， 
 model.add(Activation('relu'))
-model.add(Dropout(DROPOUT))
-model.add(Dense(N_HIDDEN))
+model.add(MaxPooling2D(pool_size=(2, 2),strides = 1))#池化层
+model.add(Dropout(0.25))#随机丢弃层
+
+
+model.add(Conv2D(64, kernel_size=(5,5),strides=2, padding='same'))#64个卷积核
 model.add(Activation('relu'))
-model.add(Dropout(DROPOUT))
+model.add(MaxPooling2D(pool_size=(2, 2),strides = 1))#池化层
+model.add(Dropout(0.25))#随机丢弃层
+model.add(Conv2D(164, kernel_size=(3,3),strides=1, padding='same'))#64个卷积核
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),strides = 1))#池化层
+model.add(Flatten())
+model.add(Dense(1024))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
 model.add(Dense(NB_CLASSES))
 model.add(Activation('softmax'))
+
+
+
 model.summary()
 
 model.compile(loss='categorical_crossentropy',
@@ -61,7 +76,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 history = model.fit(X_train, Y_train,
-                    batch_size=BATCH_SIZE, epochs=NB_EPOCH,
+                    batch_size=BATCH_SIZE, epochs=6,
                     verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
 
 score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
